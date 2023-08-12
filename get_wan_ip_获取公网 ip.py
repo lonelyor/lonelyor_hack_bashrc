f444b67465739bde6be2c8116f33dc6b775f0847
lonelyor <23398106+lonelyor@users.noreply.github.com>
2023-08-12 05:55:44 +0000

    1691819736601

--- get_wan_ip_获取公网 ip.py
+++ get_wan_ip_获取公网 ip.py
@@ -17,9 +17,15 @@ https://www.taobao.com/help/getip.php
 
 import requests
 
+url = 'http://www.3322.org/dyndns/getip'
+
+header = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3775.400 QQBrowser/10.6.4208.400'
+}
+
+
 def get_wan_ip():
-    url = 'http://www.3322.org/dyndns/getip'
-    r = requests.get(url=url)
+    r = requests.get(url=url, headers=header)
     ip = r.text
     return ip
 
